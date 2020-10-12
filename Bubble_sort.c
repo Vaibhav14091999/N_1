281352a644069a65d1148bf37a7d693d2158a341
Vaibhav14091999 <54628220+Vaibhav14091999@users.noreply.github.com>
2020-10-12 15:09:37 +0530

    Create Bubble_sort.c
    Adding C code for Bubble Sort.

---
+++ Bubble_sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+
+
+void swap(int *a, int *b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int *a, int n){
+    int i, j, flag;
+    flag =1;
+    for(i=0; flag && i<n; i++){
+            flag = 0;
+        for(j=0; j<n-i-1; j++)
+            if(a[j+1] < a[j]){
+                swap(&a[j+1], &a[j]);
+                flag = 1;
+            }
+    }
+}
+
+int main(){
+    int arr[] = {5,3,6,7,1,2,4};
+    printarray(arr,7);
+    bubbleSort(arr,7);
+    printarray(arr,7);
+    return 0;
+}
